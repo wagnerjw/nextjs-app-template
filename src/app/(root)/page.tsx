3e429341007c27b79ee28dc07171a6e5083efa8b
wagnerjw <julian@wagnerjw.dev>
2024-11-21 00:21:17 -0800

    feat: finalize home page UI

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,21 +1,15 @@
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import Link from 'next/link';
-import cheemsBless from '../../../public/cheems_bless.png';
-import deadEmoji from '../../../public/dead.png';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--JetBrains_Mono)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-center">
-        <p className="font-bold text-lg">
-          {' '}
-          ... not another nextjs starter template 🤦🏻...
-        </p>
-        <Image src={deadEmoji} alt="dead emoji" width={50} height={50} />
-        <p> ... with shadcn components and supabase! </p>
+      <main className="flex flex-col gap-8 row-start-2 items-center">
+        <p className=" text-lg">not another nextjs starter template 🤦🏻</p>
+        <p className="text-sm"> ... with shadcn components and supabase! </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="flex gap-4 items-center sm:flex-row">
           <Link href="/login">
             <Button variant="reverse">Login</Button>
           </Link>
@@ -24,10 +18,9 @@ export default function Home() {
           </Link>
         </div>
       </main>
-
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="row-start-3 pl-16 flex gap-40">
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          className="flex flex-auto items-center gap-2 hover:underline hover:underline-offset-4 hover:font-bold"
           href="https://github.com/wagnerjw/nextjs-app-template/"
           target="_blank"
           rel="noopener noreferrer"
@@ -39,21 +32,15 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Check Out The Github
+          check out the github
         </a>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4 font-semibold"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="flex flex-auto items-center gap-2 hover:underline hover:underline-offset-4 hover:font-bold"
+          href="https://vauth.command.verkada.com/embed/html/97175a8a-9d8d-4dd8-95f2-fdb9f8811b76/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          made in sunny Santa Barbara, CA 🌴
-          <Image
-            src={cheemsBless}
-            alt="cheems bless emoji"
-            width={20}
-            height={20}
-          />
+          made in Santa Barbara, CA 🌴
         </a>
       </footer>
     </div>
