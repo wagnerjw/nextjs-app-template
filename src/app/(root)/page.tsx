1f2beecd3feee78a4fba8120a7f29ade94de8119
wagnerjw <julian@wagnerjw.dev>
2024-11-20 22:57:32 -0800

    feat: build out homepage some more

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,71 +1,59 @@
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import Link from 'next/link';
+import cheemsBless from '../../../public/cheems_bless.png';
+import deadEmoji from '../../../public/dead.png';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p> NOT ANOTHER STARTER TEMPLATE ...</p>
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--JetBrains_Mono)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-center">
+        <p className="font-bold text-lg">
+          {' '}
+          ... not another nextjs starter template 🤦🏻...
+        </p>
+        <Image src={deadEmoji} alt="dead emoji" width={50} height={50} />
         <p> ... with shadcn components and supabase! </p>
+
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Button variant="default">Login</Button>
-          <Button variant="default">About</Button>
+          <Link href="/login">
+            <Button variant="reverse">Login</Button>
+          </Link>
+          <Link href="/about">
+            <Button variant="reverse">About</Button>
+          </Link>
         </div>
       </main>
 
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/wagnerjw/nextjs-app-template/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
+            src="https://nextjs.org/icons/github.svg"
+            alt="Github icon"
             width={16}
             height={16}
           />
-          Learn
+          Check Out The Github
         </a>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4 font-semibold"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
+          made in sunny Santa Barbara, CA 🌴
           <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            src={cheemsBless}
+            alt="cheems bless emoji"
+            width={20}
+            height={20}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
         </a>
       </footer>
     </div>
